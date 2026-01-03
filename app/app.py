5bfdc859d1731db07f65a3e6fd12d2327a2c148c
chetanuchiha16 <chetan16ck@gmail.com>
2026-01-03 15:22:58 +0530

    Add database configuration and enhance post management features
    - Updated .env file with PostgreSQL configuration.
    - Enhanced app.py to support post limits and added a new endpoint for creating posts.
    - Introduced config.py for managing environment settings using Pydantic.
    - Created db.py for database connection and table creation logic.
    - Defined Post schema in schema.py for data validation.
    - Updated dependencies in pyproject.toml and uv.lock for new packages.

--- app/app.py
+++ app/app.py
@@ -1,15 +1,33 @@
 from fastapi import FastAPI, HTTPException
-
+from app.schema import Post
 app = FastAPI()
 
-posts = {1: {"title": "First Post", "content": "This is the first post."}}
+posts = {
+        1: {"title": "First Post", "content": "This is the first post."},
+        2: {"title": "Second Post", "content": "This is the second post."},
+        3: {"title": "Third Post", "content": "This is the third post."},
+        4: {"title": "Fourth Post", "content": "This is the fourth post."}
+}
 
 @app.get("/all-posts")
-def get_posts():
+def get_posts(limit: int = None):
+    if limit:
+        # return first `limit` posts as a dict
+        # print (posts.items())
+        return dict(list(posts.items())[:limit])
+    
     return posts
 
+
 @app.get("/post/{post_id}")
-def ged_post(id:int):
-    if id not in posts:
+def get_post(post_id: int):
+    if post_id not in posts:
         raise HTTPException(status_code=404, detail="post not found")
-    return posts.get(id)
+    return posts[post_id]
+
+@app.post("/posts") 
+def create_post(post: Post) -> Post:
+    return {
+        post.title,
+        post.content
+    }
